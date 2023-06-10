d9ac85e6bea84941cb0d4d152f238803b47b481c
hasu <jun2077681@naver.com>
2023-06-11 01:22:50 +0900

    feat: update spring boot cors configuration

---
+++ src/main/java/com/subscribehub/app/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.subscribehub.app.config;
+
+import io.micrometer.common.lang.NonNullApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@NonNullApi
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true) // Enable credentials
+                        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers", "Access-Control-Max-Age");
+            }
+        };
+    }
+}
