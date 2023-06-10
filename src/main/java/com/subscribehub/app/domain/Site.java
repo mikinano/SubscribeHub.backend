23ebe6320ef04319a0450a4c89cb9ec8cbec0c6d
mikinano <illyasviel@kakao.com>
2023-06-10 15:56:41 +0900

    Site BaseTImeEntity 제거

--- src/main/java/com/subscribehub/app/domain/Site.java
+++ src/main/java/com/subscribehub/app/domain/Site.java
@@ -14,7 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Site extends BaseTimeEntity {
+public class Site {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "site_id")
